4b44506c8da8d1843778e3a89297b5bd9ccb7cf0
AlonzoCP <143468821+AlonzoCP@users.noreply.github.com>
2023-09-27 16:47:11 -0300

    Add files via upload

---
+++ Proyecto Unidad 2/bala.py
@@ -0,0 +1,121 @@
+import pygame, sys, globales, math
+
+class Balas (pygame.sprite.Sprite):
+    #Constructor
+    def __init__(self, tipo, daño, unidades):
+        super().__init__()
+        self.vGlobales = globales.Globaless()
+        self.altaura_max = 0  
+        self.ancho = 5
+        self.alto = 5
+        self.tipo = tipo
+        self.daño = daño
+        self.unidades = unidades
+        self.contador_recorrido = 1
+        self.coordenadas_altura_max = (0,-100)
+        #Tamano de la bala
+        self.image = pygame.Surface ((self.ancho,self.alto))
+
+        #obtiene el rectangulo (Sprite)
+        self.rect = self.image.get_rect()
+        self.rect.center = (500,-100)
+        self.image.fill(self.vGlobales.celeste)
+
+        self.gravedad= 9.8
+
+        
+        self.caida = False
+
+    def update(self, tanque, tanque_enemigo):
+        #solo empezara el disparo si se ejecuto la funcion disparo
+        if (self.caida == True):
+            self.caida_Bala(tanque, tanque_enemigo)
+            #moviemiento de la bala
+            self.image.fill (self.vGlobales.NEGRO)
+            self.rect.x = self.Xi + (self.velx * self.timepo)*0.5
+            self.rect.y = self.Yi + (self.vely * self.timepo + 0.5 * self.gravedad * self.timepo**2)*0.5
+            self.timepo += 0.12
+            self.contador_recorrido+=1
+        else:
+            self.retorno_bala()
+
+    def disparar (self,_angulo_grados, _angulo, _velocidad, tanque):
+        self.velx = math.sin(_angulo) * _velocidad
+        self.vely = math.cos(_angulo) * _velocidad
+        self.rect.x= tanque.rect.x 
+        self.rect.y = tanque.rect.y - tanque.alto
+        self.Xi = self.rect.centerx
+        self.Yi = self.rect.centery
+        self.angulo = _angulo_grados
+        self.timepo=0
+        self.caida = True
+        self.altaura_max = 0
+        
+        
+
+    def caida_Bala(self, tanque, tanque_enemigo):
+        #Toma el color para la colision de la bala 
+        color = self.vGlobales.celeste
+        if (self.rect.y > 0 and self.rect.x < 1280 ):
+            color = self.vGlobales.PANTALLA.get_at((self.rect.x,self.rect.y))
+            color = (color[0], color[1], color[2])
+
+        #Verifica la colision con terreno o panel
+        if (color == self.vGlobales.verde or color == self.vGlobales.grisclaro):
+            if self.tipo == self.vGlobales.bala_chica:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_chica/2),0)
+            self.caida = False
+        
+        #Verifica los rangos de la pantalla
+        if (self.rect.x >= self.vGlobales.WIDTH or self.rect.x <= 0):
+            self.caida = False
+        
+        #Verifica suelo
+        if (self.rect.x < 0 and self.rect.y > 720):
+            self.caida = False
+        
+        #Colision con el tanque propio
+        if tanque_enemigo.rect.x + tanque_enemigo.largo > self.rect.x and \
+        tanque_enemigo.rect.x < self.rect.x + self.ancho and \
+        tanque_enemigo.rect.y + tanque.alto > self.rect.y and \
+        tanque_enemigo.rect.y < self.rect.y + self.alto:
+            self.caida = False
+            if self.tipo == self.vGlobales.bala_chica:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_chica/2),0)
+                tanque_enemigo.vida = tanque_enemigo.vida - self.vGlobales.daño_bala_c
+            if self.tipo == self.vGlobales.bala_mediana:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_mediana/2),0)
+                tanque_enemigo.vida = tanque_enemigo.vida - self.vGlobales.daño_bala_m
+            if self.tipo == self.vGlobales.bala_grande:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_grande/2),0)
+                tanque_enemigo.vida = tanque_enemigo.vida - self.vGlobales.daño_bala_g
+            print("vida: ", tanque_enemigo.vida)
+
+        #Colision con el tanque enemigo
+        if tanque.rect.x + tanque.largo > self.rect.x and \
+        tanque.rect.x < self.rect.x + self.ancho and \
+        tanque.rect.y + tanque.alto > self.rect.y and \
+        tanque.rect.y < self.rect.y + self.alto:
+            self.caida = False
+            if self.tipo == self.vGlobales.bala_chica:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_chica/2),0)
+                tanque.vida = tanque.vida - self.vGlobales.daño_bala_c
+            if self.tipo == self.vGlobales.bala_mediana:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_mediana/2),0)
+                tanque.vida = tanque.vida - self.vGlobales.daño_bala_m
+            if self.tipo == self.vGlobales.bala_grande:
+                pygame.draw.circle(self.vGlobales.PANTALLA,self.vGlobales.NEGRO,(self.rect.x, self.rect.y),(self.vGlobales.bala_grande/2),0)
+                tanque.vida = tanque.vida - self.vGlobales.daño_bala_g
+            print("vida enemigo: ", tanque.vida)
+
+        #Altura maxima
+        if self.altaura_max < tanque.rect.y - self.rect.y:
+            self.altaura_max = tanque.rect.y - self.rect.y
+            self.coordenadas_altura_max = (self.rect.x, self.rect.y - 10)
+
+        if (int(self.rect.right) > self.vGlobales.WIDTH or int(self.rect.bottom) > self.vGlobales.HEIGHT): #cambia numeros
+            self.rect.centerx = self.Xi-2
+            self.rect.centery = self.Yi-2  
+            
+    def retorno_bala (self):
+        self.rect.center = (500,-100)
---
+++ Proyecto Unidad 2/mian.py
@@ -0,0 +1,124 @@
+import pygame, sys, globales, tanque, bala, random, interfaz
+from pygame.locals import *
+
+#DECLARACIONES
+pygame.init()
+interfaz = interfaz.Interfazz()
+#variables globales
+vGlobales = globales.Globaless()
+RELOJ = pygame.time.Clock()
+#pantalla
+DISPLAYSURF = vGlobales.PANTALLA 
+pygame.display.set_caption("Tanques Lovers Juego")
+
+#imagenes
+icono = pygame.image.load("imagenes/tanque.png")
+pygame.display.set_icon(icono)
+#fondo
+fondo = pygame.image.load("imagenes/fondo.png")
+DISPLAYSURF.blit(fondo, (0,0))
+
+#jugador 1
+skin1 = pygame.image.load("imagenes/skin1.png")
+skin2 = pygame.image.load("imagenes/skin2.png")
+
+#objetos en pantalla
+sprites = pygame.sprite.Group()
+tanque1 = tanque.Tankes(vGlobales.AZUL,random.randint(vGlobales.ancho_gris + 10,(vGlobales.WIDTH - vGlobales.ancho_gris)/2 + vGlobales.ancho_gris ))
+tanque2 = tanque.Tankes(vGlobales.ROJO,random.randint((vGlobales.WIDTH- vGlobales.ancho_gris)/2 + tanque1.rect.x, vGlobales.WIDTH - 10))    
+bala_g = bala.Balas(vGlobales.bala_grande, vGlobales.daño_bala_g, vGlobales.unidades_cyg)
+bala_m = bala.Balas(vGlobales.bala_mediana, vGlobales.daño_bala_m, vGlobales.unidades_m)
+bala_c = bala.Balas(vGlobales.bala_chica, vGlobales.daño_bala_c, vGlobales.unidades_cyg)
+sprites.add(tanque1)
+sprites.add(tanque2)
+sprites.add(bala_g)
+sprites.add(bala_m)
+sprites.add(bala_c)
+recorrido = []
+
+#Creacion de variables en main (por ahora)
+turno_jugador = 2
+turno_pasado = 0
+
+while True:
+    #Dibujo de la pantalla
+    DISPLAYSURF.blit(fondo,(0,0))
+    vGlobales.terreno()
+    #Interfaz
+    interfaz.interfaz()
+    
+    #proceso de cambio de turno
+    if turno_jugador == 1 and turno_pasado == 0:
+        turno_jugador = 2
+        turno_pasado = 1
+        print("Es el turno del jugador 2")
+    elif turno_jugador == 2 and turno_pasado == 0:
+        turno_jugador = 1
+        turno_pasado = 1
+        print("Es el turno del jugador 1")
+
+    #bucle para cerrar el juego
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if bala_c.caida != True:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if turno_jugador == 1:
+                    turno_pasado = interfaz.click_mouse(event.pos,bala_c,tanque1, turno_pasado, turno_jugador)
+                    recorrido.clear()
+                else:
+                    turno_pasado = interfaz.click_mouse(event.pos,bala_c, tanque2, turno_pasado, turno_jugador)
+                    recorrido.clear()
+
+        if event.type == pygame.KEYDOWN:
+            interfaz.escribir(event)
+
+    #SPRITES
+    tanque1.update()
+    tanque2.update()
+
+    if turno_jugador == 1:
+        bala_c.update(tanque1,tanque2)
+        interfaz.text_altura_maxima = str(bala_c.altaura_max) + " metros"
+        interfaz.text_surface_altura_maxima = interfaz.vGlobales.font.render(interfaz.text_altura_maxima, True, interfaz.vGlobales.NEGRO)
+        interfaz.text_surface_altura_maxima_rect = interfaz.text_surface_altura_maxima.get_rect(center = (bala_c.coordenadas_altura_max))
+
+    elif turno_jugador == 2:
+        bala_c.update(tanque2, tanque1)
+        interfaz.text_altura_maxima = str(bala_c.altaura_max) + " metros"
+        interfaz.text_surface_altura_maxima = interfaz.vGlobales.font.render(interfaz.text_altura_maxima, True, interfaz.vGlobales.NEGRO)
+        interfaz.text_surface_altura_maxima_rect = interfaz.text_surface_altura_maxima.get_rect(center = (bala_c.coordenadas_altura_max))
+    sprites.draw(DISPLAYSURF)
+    
+    #Recorrido de la bala
+    if (bala_c.contador_recorrido % 5) == 0 and bala_c.caida == True :
+        recorrido = recorrido + [(bala_c.rect.x, bala_c.rect.y)]
+    i=0
+    while (i<len(recorrido)):
+        pygame.draw.circle(DISPLAYSURF, vGlobales.NEGRO,(recorrido[i]),5)
+        i+=1
+
+    #Skins
+    DISPLAYSURF.blit(skin1, (tanque1.rect.x-15,tanque1.rect.y-10))
+    DISPLAYSURF.blit(skin2, (tanque2.rect.x-25,tanque2.rect.y-10))
+    
+    #Game over
+    if (tanque1.vida <= 0 or tanque2.vida <= 0):
+        while True:
+            interfaz.text_game_over = "GAME OVER"
+            interfaz.text_surface_game_over = interfaz.vGlobales.font.render(interfaz.text_game_over, True, interfaz.vGlobales.rojo_oscuro)
+            interfaz.text_surface_game_over_rect = interfaz.text_surface_game_over.get_rect(center = ((interfaz.vGlobales.WIDTH/2) + 140,(interfaz.vGlobales.HEIGHT/2) - 30))
+            interfaz.vGlobales.PANTALLA.blit(interfaz.text_surface_game_over, interfaz.text_surface_game_over_rect)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.quit()
+                    sys.exit()
+                    
+    #Inicio de interfaz
+    interfaz.print_interfaz()
+    interfaz.vGlobales.PANTALLA.blit(interfaz.text_surface_altura_maxima, interfaz.text_surface_altura_maxima_rect)
+    pygame.display.flip()
+    RELOJ.tick(vGlobales.FPS)
