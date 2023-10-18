bfe412ffbdaf7c45173c0150c5f3115fc041a719
thepollofu <143559060+thepollofu@users.noreply.github.com>
2023-10-18 18:06:50 -0300

    Add files via upload

--- Proyecto Unidad 2/tanque.py
+++ Proyecto Unidad 2/tanque.py
@@ -13,6 +13,9 @@ class Tankes (pygame.sprite.Sprite):
         self.largo = 15
         self.alto = 10
         self.caida = True
+        self.distancia_caida = 0
+        self.inmune= True
+        self.a=2
         self.image = pygame.Surface ((self.largo,self.alto))
         self.image.fill(_color)
         self.rect = self.image.get_rect()
@@ -26,16 +29,21 @@ class Tankes (pygame.sprite.Sprite):
         self.caida_Tanque()
         if (self.caida):
             self.rect.y += incremento
+            
+        else:
+            self.vida = self.vida - self.distancia_caida
+            print(self.vida)
+            self.distancia_caida = 0
 
     def caida_Tanque(self):
         #Colision con color del terreno
         if self.rect.y < self.vGlobales.HEIGHT - self.alto:
             self.color = self.vGlobales.PANTALLA.get_at((self.rect.midbottom[0],self.rect.midbottom[1]))
             self.color = (self.color[0], self.color[1], self.color[2])
-        if (self.color == self.vGlobales.verde):
+        if (self.color == self.vGlobales.verde or self.rect.y >= self.vGlobales.HEIGHT):
             self.caida = False
+            self.inmune = False
         else:
             self.caida = True
-        if self.rect.y >= 725:
-            self.caida = False
-    
+            if not(self.inmune):
+                self.distancia_caida += 0.5
